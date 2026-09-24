cb8ccafa1b234e14b82eef3473b76d2e8912058d
Henk Wiedig <henk@wiedig.de>
2026-09-24 08:14:59 +0200

    proper power handleing

--- package/ar8030-transport-tx/ar8030-transport-tx.mk
+++ package/ar8030-transport-tx/ar8030-transport-tx.mk
@@ -14,7 +14,7 @@
 #       >> $(O)/local.mk
 #
 # or as a plain environment variable to builder.sh/make.
-AR8030_TRANSPORT_TX_VERSION = ccddf45e6789afa62a81e1a747a58234d01a0b60
+AR8030_TRANSPORT_TX_VERSION = 02f4ed0f09b8c49a0949071016e8d056cdd2684d
 AR8030_TRANSPORT_TX_SITE = https://github.com/henkwiedig/ar8030-transport.git
 AR8030_TRANSPORT_TX_SITE_METHOD = git
 AR8030_TRANSPORT_TX_LICENSE = MIT
