ca22a7d29d08300065844770229e1c1b8c9dfd5e
Henk Wiedig <henk@wiedig.de>
2026-09-24 08:54:38 +0200

    ar8030-transport: bump

--- package/ar8030-transport-tx/ar8030-transport-tx.mk
+++ package/ar8030-transport-tx/ar8030-transport-tx.mk
@@ -14,7 +14,7 @@
 #       >> $(O)/local.mk
 #
 # or as a plain environment variable to builder.sh/make.
-AR8030_TRANSPORT_TX_VERSION = 02f4ed0f09b8c49a0949071016e8d056cdd2684d
+AR8030_TRANSPORT_TX_VERSION = dd62a75fb2e9633d95b04277b8435ffca704b901
 AR8030_TRANSPORT_TX_SITE = https://github.com/henkwiedig/ar8030-transport.git
 AR8030_TRANSPORT_TX_SITE_METHOD = git
 AR8030_TRANSPORT_TX_LICENSE = MIT
