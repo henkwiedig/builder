61915be11152fb4085fe3f63c5b56ea2b5ac0e90
Henk Wiedig <henk@wiedig.de>
2026-09-24 16:43:53 +0200

    vin readings

--- package/ar8030-transport-tx/ar8030-transport-tx.mk
+++ package/ar8030-transport-tx/ar8030-transport-tx.mk
@@ -14,7 +14,7 @@
 #       >> $(O)/local.mk
 #
 # or as a plain environment variable to builder.sh/make.
-AR8030_TRANSPORT_TX_VERSION = dd62a75fb2e9633d95b04277b8435ffca704b901
+AR8030_TRANSPORT_TX_VERSION = 2f01bc9b205c685dc22cea4a27af6dfe3c283195
 AR8030_TRANSPORT_TX_SITE = https://github.com/henkwiedig/ar8030-transport.git
 AR8030_TRANSPORT_TX_SITE_METHOD = git
 AR8030_TRANSPORT_TX_LICENSE = MIT
